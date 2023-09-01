8848cf418800b51d5608d55f5796be7e0b2e49b4
biryukvy <biryuk.vy@gmail.com>
2023-09-01 23:22:57 +0300

    add GraphQL module with Apollo's playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { TestResolver } from './test.resolver';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: false, // turn off built-in playground in favour of Apollo's playground
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    TestResolver,
+  ],
 })
 export class AppModule {}
