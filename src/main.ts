c63343809807162075137e9faa0af03a266d7406
biryukvy <biryuk.vy@gmail.com>
2023-09-13 23:52:04 +0300

    add custom logger

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import { helmetFixed } from './common/helmet-fixed';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new MyLogger(),
+  });
   app.use(helmetFixed);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
